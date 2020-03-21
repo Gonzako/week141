844a20700b9d0dff957b1ccf48aa0baaf01378ab
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-03-21 18:10:22 +0200

    Use GameObject event instead of general.

--- Assets/Scripts/Game/Collectible.cs
+++ Assets/Scripts/Game/Collectible.cs
@@ -1,23 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ScriptableObjectArchitecture;
 
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private Sprite _collectableSprite;
     [SerializeField] private string _name;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+      
+    [SerializeField] private GameEvent _onCollectedEvent = default(GameEvent);
+    [SerializeField] private IntReference _collectablesAmount = default(IntReference);
     
+    private void OnTriggerEnter2D(Collider2D other) {
+       if(other.tag == "Player"){
+           _onCollectedEvent.Raise();
+           _collectablesAmount.Value += 1;
+       }
+    } 
 }
